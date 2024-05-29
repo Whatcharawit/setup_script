ada74309657683ca0f1f2d546301315ba926b5f7
Whatchawrawit <whatcharawit@swiftdynamics.co.th>
2024-05-29 16:44:21 +0700

    compose

--- install_chirpstack.bash
+++ install_chirpstack.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 sudo service docker restart
 echo "docker restarted!"
-docker-compose --version
+sudo docker-compose --version
 
 git clone https://github.com/brocaar/chirpstack-docker.git
 
