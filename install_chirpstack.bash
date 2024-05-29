e07e5eac41a5cebcfc6b91179b80b2018ef8f3cc
Whatchawrawit <whatcharawit@swiftdynamics.co.th>
2024-05-29 16:17:09 +0700

    chirpstack update

--- install_chirpstack.bash
+++ install_chirpstack.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 cd 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+docker-compose --version
 
 git clone https://github.com/brocaar/chirpstack-docker.git
 
-cp ./setup-script/docker-compose.yml ./chirpstack-docker/docker-compose.yml
-cp ./setup-script/region_as923.toml ./chirpstack-docker/region_as923.toml
-p ./setup-script/chirpstack-gateway-bridge.toml ./chirpstack-docker/chirpstack-gateway-bridge.toml
+cd setup-script
+cp docker-compose.yml ~/chirpstack-docker/docker-compose.yml
+cp region_as923.toml ~/chirpstack-docker/region_as923.toml
+cp chirpstack-gateway-bridge.toml ~/chirpstack-docker/chirpstack-gateway-bridge.toml
 
+cd 
 cd chirpstack-docker
 sudo docker-compose up -d
 echo "Chirp Stack installed!"
