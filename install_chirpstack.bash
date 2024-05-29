5fb4f1165b89aeb32296a0a333709e89615f0757
Whatchawrawit <whatcharawit@swiftdynamics.co.th>
2024-05-29 16:32:40 +0700

    #

--- install_chirpstack.bash
+++ install_chirpstack.bash
@@ -7,6 +7,8 @@ docker-compose --version
 git clone https://github.com/brocaar/chirpstack-docker.git
 
 ls
+cd setup_script
+ls
 cp docker-compose.yml ~/chirpstack-docker/docker-compose.yml
 cp region_as923.toml ~/chirpstack-docker/configuration/chirpstack/region_as923.toml
 cp chirpstack-gateway-bridge.toml ~/chirpstack-docker/configuration/chirpstack-gateway-bridge/chirpstack-gateway-bridge.toml
