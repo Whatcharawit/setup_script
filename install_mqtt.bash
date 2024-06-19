539f37a4514822d8defeb20ec893d1ce24294e80
Whatchawrawit <whatcharawit@swiftdynamics.co.th>
2024-06-19 23:03:59 +0700

    install mqtt on docker

--- install_mqtt.bash
+++ install_mqtt.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd 
+cd setup_script/mqtt
 
+# In case you don't have docker-compose you can install it
+sudo apt install docker-compose -y
 
-nano config/mosquitto.conf
+sudo docker-compose -p mqtt up -d
 
+echo "Install MQTT successful!"
